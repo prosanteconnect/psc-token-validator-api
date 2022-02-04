6fb77e7297c903e5f6bd2fb4f1c59b5b44601513
cjuillard <claire.juillard.ext@esante.gouv.fr>
2022-02-04 14:03:06 +0100

    Init project. WS skeleton

---
+++ src/main/java/fr/ans/psc/PscTokenValidatorApiApplication.java
@@ -0,0 +1,13 @@
+package fr.ans.psc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PscTokenValidatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PscTokenValidatorApiApplication.class, args);
+	}
+
+}

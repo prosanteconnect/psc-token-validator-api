c2fe3e4dadd7e7e756511f9bc8e148a3d1a72f59
cjuillard <claire.juillard.ext@esante.gouv.fr>
2022-02-04 14:21:51 +0100

    API: /tokenvalidator/{token}

---
+++ src/main/java/fr/ans/psc/tokenvalidator/api/TokenvalidatorDelegateImpl.java
@@ -0,0 +1,17 @@
+package fr.ans.psc.tokenvalidator.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class TokenvalidatorDelegateImpl implements TokenvalidatorApiDelegate {
+
+	@Override
+	public ResponseEntity<String> tokenValidator(String token){
+		return new ResponseEntity<>(HttpStatus.BANDWIDTH_LIMIT_EXCEEDED);
+	}
+}
